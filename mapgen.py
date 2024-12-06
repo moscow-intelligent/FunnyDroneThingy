63d0a400e76e58cb0e3000bfe048ca00532fd9c2
UranBee <uranbaranow@gmail.com>
2024-12-06 18:51:58 +0300

    mapgen

--- mapgen.py
+++ mapgen.py
@@ -1,9 +1,13 @@
+from main import RECT_SIZE
+from classes import Tile, Tiles
+import random
+
 
 def generate_map(width: int, height: int):
     tile_map = []
-    for y in range(0, height, TILE_SIZE):
+    for y in range(0, height, RECT_SIZE):
         row = []
-        for x in range(0, width, TILE_SIZE):
+        for x in range(0, width, RECT_SIZE):
             # Generate clusters
             cluster_size = random.randint(1, 5)
             start_x = x
@@ -12,10 +16,10 @@ def generate_map(width: int, height: int):
                 tile_type = random.choice(list(Tiles)) 
                 tile = Tile(start_x, start_y, tile_type)
                 row.append(tile)
-                start_x += TILE_SIZE
+                start_x += RECT_SIZE
                 if start_x >= width:
                     start_x = x
-                    start_y += TILE_SIZE
+                    start_y += RECT_SIZE
         tile_map.append(row)
     return tile_map
 
