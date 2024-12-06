4ad7d0ce5ba0a81fd29a65645a008fb100d4e681
demka <demka@duck.com>
2024-12-06 18:19:32 +0300

    Added enum

--- classes.py
+++ classes.py
@@ -1,5 +1,11 @@
 from pygame import Rect
 from enum import Enum
+
+class Tiles(Enum):
+    GROUND = 1
+    WATER = 2
+    CONCRETE = 3
+    GRASS = 4
+
 class Tile(Rect):
-    def __init__(self, x: int, y: int, type:Enum):
-        ...
+    def __init__(self, x: int, y: int, type:Tiles):
