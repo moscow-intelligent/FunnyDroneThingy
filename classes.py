218f6e764e56fe1700a23ee60945f39b5712ede7
demka <demka@duck.com>
2024-12-06 18:50:23 +0300

    123

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 from typing import Self
-from pygame import Rect, image, transform
+from pygame import Rect, image, transform, K_RIGHT, K_LEFT, K_UP, K_DOWN
+from math import sqrt
 from enum import Enum
 
 
@@ -22,6 +23,34 @@ class Tile(Rect):
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
+class Player():
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.width = 50
+        self.height = 50
+
+    def move(self, keys):
+        # Movement vector
+        move_x = keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+        move_y = keys[pygame.K_DOWN] - keys[pygame.K_UP]
+
+        # Calculate the length of the movement vector
+        length = math.sqrt(move_x**2 + move_y**2)
+
+        # Normalize the vector if it's not zero
+        if length != 0:
+            move_x /= length
+            move_y /= length
+
+        # Update position based on speed
+        self.x += move_x * self.speed
+        self.y += move_y * self.speed
+
+    def draw(self, surface):
+        # Draw the character as a rectangle
+        pygame.draw.rect(surface, (0, 0, 255), (self.x, self.y, self.width, self.height))
 
 class World:
     def __init__(self):
@@ -35,6 +64,9 @@ class World:
     #             tile.draw(surface)
     #
     #
+
+
+
 def get_tile_path(t: Tiles):
     return {Tiles.WATER:'map_tiles/water.png',
             Tiles.GROUND:'map_tiles/ground.png',
